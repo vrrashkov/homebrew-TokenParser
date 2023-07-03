315203ca1f2c24bd6d2b17301095f8bd2fa8ac12
Vladislav <mrcrozer@gmail.com>
2023-07-03 17:34:41 +0300

    v3.1.1

--- Formula/tokenparser.rb
+++ Formula/tokenparser.rb
@@ -3,7 +3,7 @@ class Tokenparser < Formula
   homepage "https://github.com/vrrashkov/TokenParser"
   url "https://github.com/vrrashkov/TokenParser/releases/download/v3.1.1/design_token_parser-mac.tar.gz"
   sha256 "560ff17d23735324f077b383bda43662e8420888f540e7392dc0d43570e73050"
-  version "3.1.0"
+  version "3.1.1"
 
   def install
     bin.install "design_token_parser"
