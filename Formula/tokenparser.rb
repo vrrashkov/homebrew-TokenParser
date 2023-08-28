6932ed05daa1513a9738ddfe45d33bbb913d4bea
Vladislav <mrcrozer@gmail.com>
2023-08-28 14:25:29 +0300

    3.1.7

--- Formula/tokenparser.rb
+++ Formula/tokenparser.rb
@@ -2,13 +2,13 @@ class Tokenparser < Formula
   desc "Token parser for figma tokens"
   homepage "https://github.com/vrrashkov/TokenParser"
   if Hardware::CPU.intel?
-    url "https://github.com/vrrashkov/TokenParser/releases/download/v3.1.5/design_token_parser-mac_86.tar.gz"
-    sha256 "d55a67c81ca4ee4ea589ce815516caa3080c472a3d6d25c009d504ed84e3090d"
+    url "https://github.com/vrrashkov/TokenParser/releases/download/v3.1.7/design_token_parser-mac_86.tar.gz"
+    sha256 "be0ecdf40f0bb75bb379ad77be3c25a08e4cdc20df995c964ec1701ec0ba3113"
   else
-    url "https://github.com/vrrashkov/TokenParser/releases/download/v3.1.5/design_token_parser-mac.tar.gz"
-    sha256 "5aa78384d0e6c27b3b10c238b6175097b10ec852245c0d5dc2f37264f89c5f95"
+    url "https://github.com/vrrashkov/TokenParser/releases/download/v3.1.7/design_token_parser-mac.tar.gz"
+    sha256 "8854e798801ec7063f8cf6bebdc3141e85f02ec502abb0e130633913061c6210"
   end
-  version "3.1.6"
+  version "3.1.7"
 
   def install
     bin.install "design_token_parser"
