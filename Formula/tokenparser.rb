ddb661d88605a8491dadd1d92829ed01e67d8473
Vladislavl <mrcrozer@gmail.com>
2023-10-05 12:10:46 +0300

    v3.2.0-test2

--- Formula/tokenparser.rb
+++ Formula/tokenparser.rb
@@ -1,12 +1,12 @@
 class Tokenparser < Formula
   desc "Token parser for figma tokens"
   homepage "https://github.com/vrrashkov/TokenParser"
-  version "3.2.0,v3.2.0-ci"
+  version "3.2.0"
   if Hardware::CPU.intel?
-    url "https://github.com/vrrashkov/TokenParser/releases/download/#{version.after_comma}/design_token_parser-darwin-amd64.tar.gz"
+    url "https://github.com/vrrashkov/TokenParser/releases/download/v3.2.0-ci/design_token_parser-darwin-amd64.tar.gz"
     sha256 "3fc9f91437806542bff84538979ed35dc96106818e86d0fb734edb2e6a71992f"
   else
-    url "https://github.com/vrrashkov/TokenParser/releases/download/#{version.after_comma}/design_token_parser-darwin-arm64.tar.gz"
+    url "https://github.com/vrrashkov/TokenParser/releases/download/v3.2.0-ci/design_token_parser-darwin-arm64.tar.gz"
     sha256 "00e5536c30bb83d39c66761d328c537b8fad918a4b716e78320a85dfc826ba5c"
   end
 
